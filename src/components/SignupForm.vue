c7bc9b85a2a31c19e5152175a1f8d78d32ff0084
shuff <shuff@gaig.com>
2023-05-26 10:22:04 -0400

    up through lesson 46

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import {computed, ref} from "vue";
 import FormInput from "./FormInput.vue";
+import {NewUser} from "../users";
 import {validate, length, required} from "../validation";
-
+import {useUsers} from '../stores/users'
+import {useModal} from "../composables/modal";
 
 const username = ref('')
 const usernameStatus = computed(() => {
@@ -12,12 +14,32 @@ const password = ref('')
 const passwordStatus = computed(() => {
         return validate(password.value, [required, length({min: 10, max: 40})])
     })
+const isInvalid = computed(()=> {
+    return (!usernameStatus.value.valid || !passwordStatus.value.valid)
+    })
+
+const usersStore = useUsers()
+const modal = useModal()
+async function handleSubmit () {
+    if(isInvalid.value){
+        return
+    }
+    const newUser: NewUser = {
+        username: username.value,
+        password: password.value,
+    }
+    try{
+        await usersStore.createUser(newUser)
+    } catch (e){}
+    modal.hideModal()
+}
 </script>
 
 <template>
-    <form class="form">
-        <FormInput name="Username" v-model="username" :status="usernameStatus" />
-        <FormInput name="Password" v-model="password" :status="passwordStatus" />
+    <form class="form" @submit.prevent="handleSubmit">
+        <FormInput name="Username" v-model="username" :status="usernameStatus" type="text" />
+        <FormInput name="Password" v-model="password" :status="passwordStatus" type="password"/>
+        <button class="button" :disabled="isInvalid">Submit</button>
     </form>
 </template>
 
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import {today, thisWeek, thisMonth, Post} from '../posts'
-import * as path from "path"
+import {NewUser, User} from "../users";
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
 const allPosts = [today, thisWeek, thisMonth]
+const allUsers: User[] = []
 app.get("/posts", (req, res) => {
     res.json(allPosts)
 })
@@ -18,6 +19,14 @@ app.post<{}, {}, Post>("/posts", (req, res) => {
     allPosts.push(post)
     res.json(post)
 })
+
+app.post<{}, {}, NewUser>("/users", (req, res) => {
+    const user : User = {...req.body, id: (Math.random() * 100000).toFixed()}
+    allUsers.push(user)
+    const {password, ...rest} = user
+    res.json(rest)
+})
+
 
 app.listen(8000, () => {
     console.log('Listening on port 8000')
